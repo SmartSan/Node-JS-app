3e7a1a44a6caf52831c86323d1a08f9b71e06bf5
smart100boyz@gmail.com <smart100boyz@gmail.com>
2018-06-26 16:08:41 +0530

    new function added

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ function something()
 	console.log('I\'m something');
 	console.log('Bug Fixed!!!');
 }
-		
+	
+function version1()
+{
+	console.log('The Version 1 work is going on...');
+	console.log('So far So Good!');
+}
 		
 		console.log("Hello world");
 something();
 aBigNewFeature();
+version1();
